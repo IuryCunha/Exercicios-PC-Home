dda5512c52c3422c879f0821b7ab9e449b684cd8
IuryCunha <iurycunha1700@gmail.com>
2025-12-13 12:15:30 -0300

    enviando ex 84 pc home

--- PHP_ex_apostila/Ex_84.php
+++ PHP_ex_apostila/Ex_84.php
@@ -7,27 +7,15 @@ contendo apenas os dados das pessoas menores de idade.*/
 $vetoresNome = [];
 $vetoresIdade = [];
 
-for ($i = 1; $i <= 3; $i++) {
-    $vetoresNome [$i] = readline("Digite o nome: ");
+for ($i = 0; $i < 3; $i++) {
+    $vetoresNome [$i] = readline("Digite o nome " . ($i+1) . ": ");
     $vetoresIdade [$i] = (int) readline("Digite a idade: ");
-
-
 }
 
-echo "Nomes: ";
+echo "Nomes: " . PHP_EOL;
 
-if ($vetoresIdade [$i] < 18) {
-
-    foreach ($vetoresNome as $nome) {
-    echo $nome . ", ";
-    }
-}
-echo PHP_EOL;
-
-echo "Idades: ";
-
-if ($vetoresIdade [$i] < 18) {
-    foreach ($vetoresIdade as $idade) {
-        echo $idade . ", "; 
+for ($i = 0; $i < 3; $i++) {
+    if ($vetoresIdade[$i] < 18) {
+        echo "Nome: $vetoresNome[$i], Idade: $vetoresIdade[$i]" . PHP_EOL;
     }
 }
