1f9cec9541396fefffb75d3d78e9484d866a0dfb
IuryCunha <iurycunha1700@gmail.com>
2025-12-23 14:20:08 -0300

    enviando ex 94 apostila

--- PHP_ex_apostila/Ex_94.php
+++ PHP_ex_apostila/Ex_94.php
@@ -8,3 +8,35 @@ Obs: Use os exercícios 70 e 75 para te ajudar na solução
 Ex:
 Fibonacci(5) vai gerar 1 >> 1 >> 2 >> 3 >> 5 >> FIM
 Fibonacci(9) vai gerar 1 >> 1 >> 2 >> 3 >> 5 >> 8 >> 13 >> 21 >> 34 >> FIM */
+
+$sequencia = (int) readline("informe o termo da sequencia: ");
+
+function Fibonacci($sequencia) {
+
+    $vetores = [];
+
+    $a = 1;
+    $b = 1;
+
+
+
+    for ($i = 0; $i < $sequencia; $i++) {
+        if ($i == 0 || $i == 1) {
+            $vetores [$i] = 1;
+        }
+        else {
+            $c = $a + $b;
+            $vetores [$i] = $c;
+            $a = $b;
+            $b = $c;
+        }
+
+    }
+
+    foreach ($vetores as $valor) {
+        echo $valor . " >> ";
+    }
+        echo " Fim" . PHP_EOL; 
+}
+
+Fibonacci($sequencia);
