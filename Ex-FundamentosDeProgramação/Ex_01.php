b15ece79f06e5c1b2be3ff86897ecd19b0a62e15
IuryCunha <iurycunha1700@gmail.com>
2026-01-24 23:45:54 -0300

    enviando ex apostila 2

---
+++ Ex-FundamentosDeProgramação/Ex_01.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Crie uma função que retorne a string "Olá," concatenada com o argumento text (a ser passado para a função) e com ponto de exclamação "!" no final. */
+
+function cumprimentar($nome) { // $nome é o parâmetro
+    echo "Olá, $nome!";
+}
+cumprimentar("João");
