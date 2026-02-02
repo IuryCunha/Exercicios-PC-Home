ad793dac0a2582dc72cdbfeca5b4e537369d8732
IuryCunha <iurycunha1700@gmail.com>
2026-02-01 23:12:44 -0300

    enviando ex 05 fundamentos

---
+++ Ex-FundamentosDeProgramação/Ex_05.php
@@ -0,0 +1,22 @@
+<?php
+
+/* Crie uma função que receba 2 números e retorne se o primeiro é maior ou igual ao segundo
+*/
+
+function MaiorOuMenor($numero1, $numero2) 
+{
+
+    return $numero1 >= $numero2;
+    
+}
+
+$numero1 = (int) readline("Digite o primeiro número: ");
+$numero2 = (int) readline("Digite o segundo número: ");
+
+$resultado = MaiorOuMenor($numero1, $numero2);
+
+if ($resultado) {
+    echo "O primeiro número é maior ou igual ao segundo.";
+} else {
+    echo "O primeiro número é menor que o segundo.";
+}
