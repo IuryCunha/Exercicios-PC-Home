4437a43dcf058b39dc409e1655fcc98d72f638d2
IuryCunha <iurycunha1700@gmail.com>
2026-06-01 17:58:18 -0300

    adicionando comentários

--- PHP_ex_apostila/Ex_24.php
+++ PHP_ex_apostila/Ex_24.php
@@ -1,12 +1,17 @@
 <?php
 
+/* 24) Faça um algoritmo que pergunte a distância que um passageiro deseja
+percorrer em Km. Calcule o preço da passagem, cobrando R$0.50 por Km para
+viagens até 200Km e R$0.45 para viagens mais longas. */
+
+$passagem = 0;
+
 echo "qual distância voce deseja percorrer? ";
 $distancia = readline();
 
-if ($distancia <= 50) {
-    $passagem = $distancia * 1;
-}
-elseif ($distancia > 50 and $distancia <= 100) {
-    $passagem = $distancia * 2;
+if ($distancia <= 200) {
+    $passagem = $distancia * 0.50;
+} else {
+    $passagem = $distancia * 0.45;
 }
 echo "o preço da passagem é de R$ $passagem";
