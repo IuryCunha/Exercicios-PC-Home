d103f2876fef7041062d17b6d6e67349b5507f74
IuryCunha <iurycunha1700@gmail.com>
2026-06-02 11:24:15 -0300

    adicionando comentários

--- PHP_ex_apostila/Ex_26.php
+++ PHP_ex_apostila/Ex_26.php
@@ -1,5 +1,11 @@
 <?php
 
+/* 26) Escreva um algoritmo que leia dois números inteiros e compare-os, mostrando
+na tela uma das mensagens abaixo:
+ - O primeiro valor é o maior
+ - O segundo valor é o maior
+ - Não existe valor maior, os dois são iguais */
+
 echo "Primeiro número: ";
 $numero_1 = readline();
 echo "Segundo número: ";
